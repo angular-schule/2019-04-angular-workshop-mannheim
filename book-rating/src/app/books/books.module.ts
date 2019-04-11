b7cc765babdab13401fd885259c658edb5e52688
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2019-04-11 15:40:46 +0200

    book details

--- book-rating/src/app/books/books.module.ts
+++ book-rating/src/app/books/books.module.ts
@@ -7,13 +7,15 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { BookComponent } from './book/book.component';
 import { NumberArrayPipe } from './shared/number-array.pipe';
 import { CreateBookComponent } from './create-book/create-book.component';
+import { BookDetailsComponent } from './book-details/book-details.component';
 
 @NgModule({
   declarations: [
     DashboardComponent,
     BookComponent,
     NumberArrayPipe,
-    CreateBookComponent
+    CreateBookComponent,
+    BookDetailsComponent
   ],
   imports: [
     CommonModule,
